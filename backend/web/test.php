2c7b9f97ed94e3e0f43c3aab1af06089d5fda0b0
acumencraft <managua@live.com>
2025-07-29 11:11:18 +0400

    Remove redundant root web directory and clean up duplicate database table

---
+++ backend/web/test.php
@@ -0,0 +1,5 @@
+<?php
+echo "BACKEND APPLICATION - Admin Domain Working!";
+echo "<br>Server: " . $_SERVER['SERVER_NAME'];
+echo "<br>Request URI: " . $_SERVER['REQUEST_URI'];
+?>
---
+++ frontend/web/test.php
@@ -0,0 +1,5 @@
+<?php
+echo "FRONTEND APPLICATION - Main Domain Working!";
+echo "<br>Server: " . $_SERVER['SERVER_NAME'];
+echo "<br>Request URI: " . $_SERVER['REQUEST_URI'];
+?>
